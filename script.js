3f2c6df3cbfd35e414acf19eb03265eb46c56065
Kuyamoakel <Alfredalmozara@gmail.com>
2026-05-28 02:51:08 +0800

    feat: change the fonts, when clicking other buttons disapper, also include session counter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // variables
 const timer = document.getElementById("timer");
-const breakDisplay = document.querySelector(".break");
+const label = document.getElementById("label");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 const restartButton = document.getElementById("restart");
@@ -12,10 +12,11 @@ const NatureSound = new Audio("assets/audio/NatureSound.mp3");
 const timesUp = new Audio("assets/audio/times-up.mp3");
 
 
-let timeleft = 25 * 60;
-let breakTime = 5 * 60;
+let timeleft = 1 * 5;
+let breakTime = 1 * 5;
 let interval = null;
 let isBreak = false;
+let sessionCount = 0;
 
 function clickingSound() {
     clickSound.play();
@@ -27,6 +28,39 @@ function stopAllSounds() {
 
     timesUp.pause();
     timesUp.currentTime = 0;
+}
+
+function unHideButtons() {
+    pauseButton.classList.remove("hidden");
+    restartButton.classList.remove("hidden");
+}
+
+function hideButtons() {
+    pauseButton.classList.add("hidden");
+    restartButton.classList.add("hidden");
+}
+
+function backStart() {
+    hideButtons();
+    startButton.classList.remove("start-hidden");
+}
+
+function updateSessionDisplay() {
+    document.getElementById("session-num").textContent = sessionCount + 1;
+
+    for (let i = 1; i <= 4; i++) {
+        const dot = document.getElementById("dot-" + i);
+        dot.classList.remove("done", "active");
+
+        if (i <= sessionCount) {
+            dot.classList.add("done");
+        }
+        else if (i === sessionCount + 1) {
+            dot.classList.add("active");
+        }
+    }
+
+
 }
 
 function updateTimer() {
@@ -42,7 +76,11 @@ function updateTimer() {
 function startTimer() {
     if (interval !== null) return;
 
+    NatureSound.loop = true;
     NatureSound.play();
+    
+    startButton.classList.add("start-hidden");
+    unHideButtons();
 
     interval = setInterval(() => {
 
@@ -66,7 +104,7 @@ function startTimer() {
 function pauseTimer() {
     NatureSound.pause();
     NatureSound.currentTime = 0;
-
+    backStart();
     clearInterval(interval);
     interval = null;
 }
@@ -74,9 +112,9 @@ function pauseTimer() {
 function resetTimer() {
     pauseTimer();
     stopAllSounds();
+    backStart();
     isBreak = false;
     timeleft = 25 * 60;
-    breakDisplay.classList.add("hidden")
     updateTimer();
 }
 
@@ -84,17 +122,20 @@ function switchMode() {
     isBreak = !isBreak;
 
     if (isBreak) {
+        sessionCount++;
         timeleft = breakTime;
-        breakDisplay.classList.remove("hidden");
-        breakDisplay.textContent = ("Break");
+        label.textContent = "BREAK TIME";
         timesUp.play();
+        updateSessionDisplay();
     } else {
+        if (sessionCount >= 4) sessionCount = 0;
         timeleft = 25 * 60;
-        breakDisplay.classList.add("hidden");
-
+        label.textContent = "FOCUS SESSION";
+        
         NatureSound.loop = true;
         NatureSound.currentTime = 0;
         NatureSound.play();
+        updateSessionDisplay();
     }
 
     updateTimer();
@@ -105,3 +146,4 @@ pauseButton.addEventListener("click", pauseTimer);
 restartButton.addEventListener("click", resetTimer);
 
 updateTimer();
+updateSessionDisplay();
